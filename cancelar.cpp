079c7b53202780811ac4753af1763fc78c34c589
katia250 <carnet@uca.edu.sv>
2026-06-24 03:32:55 -0300

    cancelarPedido

---
+++ cancelar.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include "cancelar.h"
+
+using namespace std;
+
+void cancelarPedido (string pedido[], double precios[], int &cantidad, float &total){
+    system ("cls"); //Limpia la pantalla 
+        if (cantidad >0){
+            for(int i=0; i<cantidad; i++){
+                pedido[i]="";
+                precios[i]=0;
+            }
+        cantidad=0;
+        total=0;
+
+        cout<<"\n Pedido cancelado.\n"<<endl;
+    }
+    else{
+        cout<<"\n No hay pedido para cancelar. \n"<<endl;
+    }
+}
