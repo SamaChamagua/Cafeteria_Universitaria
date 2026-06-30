4321aa05585782cbc53bbdde870cae107841fe36
SamaChamagua <00218826@uca.edu.sv>
2026-06-29 20:32:10 -0600

    Otro arreglo a funcion pedidos.cpp TT

--- pedidos.cpp
+++ pedidos.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include "pedidos.h"
 
 using namespace std;
@@ -24,30 +25,35 @@ void realizarPedido(string pedido[], float precios[], int &cantidad, float &tota
         cin>>CatSnacks;
 
         if (CatSnacks == 1){
+            for (int i = 0; i < cantidad; i++) {}
             pedido[cantidad]="Papa Lays";
             precios[cantidad]=0.65;
             total= total + 0.65;
             cantidad++;
         }
         else if (CatSnacks == 2){
+            for (int i = 0; i < cantidad; i++) {}
             pedido[cantidad]="Galleta Oreo";
             precios[cantidad]=0.50;
             total= total + 0.50;
             cantidad++;
         }
         else if (CatSnacks == 3){
+            for (int i = 0; i < cantidad; i++) {}
             pedido[cantidad]="Galleta trigo";
             precios[cantidad]=0.50;
             total= total + 0.50;
             cantidad++;
         }
         else if (CatSnacks == 4){
+            for (int i = 0; i < cantidad; i++) {}
             pedido[cantidad]="Cheetos";
             precios[cantidad]=0.75;
             total= total + 0.75;
             cantidad++;
         }
         else if (CatSnacks == 5){
+            for (int i = 0; i < cantidad; i++) {}
             pedido[cantidad]="Nachos";
             precios[cantidad]=0.25;
             total= total + 0.25;
@@ -72,30 +78,35 @@ void realizarPedido(string pedido[], float precios[], int &cantidad, float &tota
         cin>>CatBebidas;
 
         if (CatBebidas == 1){
+            for (int i = 0; i < cantidad; i++) {}
             pedido[cantidad]="Agua";
             precios[cantidad]=0.50;
             total= total + 0.50;
             cantidad++;
         }
         else if (CatBebidas == 2){
+            for (int i = 0; i < cantidad; i++) {}
             pedido[cantidad]="Jugo";
             precios[cantidad]=0.75;
             total= total + 0.75;
             cantidad++;
         }
         else if (CatBebidas == 3){
+            for (int i = 0; i < cantidad; i++) {}
             pedido[cantidad]="Refresco";
             precios[cantidad]=0.85;
             total= total + 0.85;
             cantidad++;
         }
         else if (CatBebidas == 4){
+            for (int i = 0; i < cantidad; i++) {}
             pedido[cantidad]="Cafe";
             precios[cantidad]=1.00;
             total= total + 1.00;
             cantidad++;
         }
         else if (CatBebidas == 5){
+            for (int i = 0; i < cantidad; i++) {}
             pedido[cantidad]="Te";
             precios[cantidad]=0.75;
             total= total + 0.75;
@@ -120,30 +131,35 @@ void realizarPedido(string pedido[], float precios[], int &cantidad, float &tota
         cin>>CatComida;
 
         if (CatComida == 1){
+            for (int i = 0; i < cantidad; i++) {}
             pedido[cantidad]="Croissant de pollo";
             precios[cantidad]=2.50;
             total= total + 2.50;
             cantidad++;
         }
         else if (CatComida == 2){
+            for (int i = 0; i < cantidad; i++) {}
             pedido[cantidad]="Sandwich";
             precios[cantidad]=1.50;
             total= total + 1.50;
             cantidad++;
         }
         else if (CatComida == 3){
+            for (int i = 0; i < cantidad; i++) {}
             pedido[cantidad]="Sopa de Frijoles";
             precios[cantidad]=1.75;
             total= total + 1.75;
             cantidad++;
         }
         else if (CatComida == 4){
+            for (int i = 0; i < cantidad; i++) {}
             pedido[cantidad]="Ensalada";
             precios[cantidad]=1.25;
             total= total + 1.25;
             cantidad++;
         }
         else if (CatComida == 5){
+            for (int i = 0; i < cantidad; i++) {}
             pedido[cantidad]="Tacos";
             precios[cantidad]=2.00;
             total= total + 2.00;
@@ -161,8 +177,7 @@ void realizarPedido(string pedido[], float precios[], int &cantidad, float &tota
         break;
 }
 
-cout<<"Su total a pagar es: $"<<total<<"\n";
-
+cout << "Total acumulado: $" << total << "\n";
 
 return;
 }
