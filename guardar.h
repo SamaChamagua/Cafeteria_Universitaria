06acec383ca5eef5c5b3df1de632ead453b898c7
Vero-L031 <00117526@uca.edu.sv>
2026-07-07 14:33:14 -0600

    main-menu

--- guardar.h
+++
@@ -1,9 +0,0 @@
-#ifndef guardar_H    
-#define guardar_H
-
-#include <string>
-using namespace std;
-
-void guardarPedido(string pedido[], float precios[], int cantidades[], int &cantidad, float &total);
-
-#endif 
--- pedidos.h
+++
@@ -1,9 +0,0 @@
-#ifndef pedidos_H    
-#define pedidos_H
-
-#include <string>
-using namespace std;
-
-void realizarPedido(string pedido[], float precios[], int &cantidad, float &total);
-
-#endif 
